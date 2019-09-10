19b498a0e06c480fddc7f87f48c0cf43b78aff08
dragkco <55118100+dragkco@users.noreply.github.com>
2019-09-10 02:08:29 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(auth.token);
+client.on('message', msg => {
+  if (msg.content === '$beanos') {
+    msg.reply('https://cdn.discordapp.com/attachments/595627324255240192/618259005806215196/Z.png');
+  }
+});

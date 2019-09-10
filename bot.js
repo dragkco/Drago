a1b2a2480728c51fa5529baf9dc62145ec0f6aab
dragkco <55118100+dragkco@users.noreply.github.com>
2019-09-10 02:29:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,29 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
+ 
+
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+
+    console.log('I am ready!');
+
 });
 
-client.login(process.env.token);
-client.on('message', msg => {
-  if (msg.content === '$beanos') {
-    msg.reply('https://cdn.discordapp.com/attachments/595627324255240192/618259005806215196/Z.png');
-  }
+ 
+
+client.on('message', message => {
+
+    if (message.content === '$beanos') {
+
+       message.reply('https://cdn.discordapp.com/attachments/595627324255240192/618259005806215196/Z.png');
+
+       }
+
 });
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
